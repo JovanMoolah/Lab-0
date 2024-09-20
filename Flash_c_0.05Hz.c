bf1f8b0a4b6e266979fb14ef1a0ee16e074029d4
JovanMoolah <145720096+JovanMoolah@users.noreply.github.com>
2024-09-19 20:39:45 -0400

    Add files via upload

---
+++ Flash_c_0.05Hz.c
@@ -0,0 +1,16 @@
+
+
+
+#include <xc.h>
+
+int main(void) {
+    TRISBbits.TRISB6 = 0;
+    while (1)                   // infinite loop that restarts when code in block is executed 
+    {
+        LATBbits.LATB6 =1;      // pin 6 on Port B is set high
+        _delay (50);           //delay time for 0.5 seconds
+        LATBbits.LATB6 = 0;    // pin 6 on Port B is set low
+        _delay (50);          // delay time for 0.5 seconds
+    }
+    return 0;
+}
