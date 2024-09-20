2b79a557908295d5766f1bf89bd1fa0c7fd290f8
JovanMoolah <145720096+JovanMoolah@users.noreply.github.com>
2024-09-19 21:10:24 -0400

    Update Flash_c_0.05Hz.c

--- Flash_c_0.05Hz.c
+++ Flash_c_0.05Hz.c
@@ -8,9 +8,9 @@ int main(void) {
     while (1)                   // infinite loop that restarts when code in block is executed 
     {
         LATBbits.LATB6 =1;      // pin 6 on Port B is set high
-        _delay (25);           //delay time for 0.025 seconds
+        _delay (25000);           //delay time for 0.025 seconds
         LATBbits.LATB6 = 0;    // pin 6 on Port B is set low
-        _delay (25);          // delay time for 0.025 seconds
+        _delay (25000);          // delay time for 0.025 seconds
     }
     return 0;
 }
