5cff375cfd5ba7e5b3fb060fd1477b0fb5fb1da2
JovanMoolah <145720096+JovanMoolah@users.noreply.github.com>
2024-09-19 21:10:54 -0400

    Update LEDFlash.c

--- LEDFlash.c
+++ LEDFlash.c
@@ -8,9 +8,9 @@ int main(void) {
     while (1)                   // infinite loop that restarts when code in block is executed 
     {
         LATBbits.LATB6 =1;      // pin 6 on Port B is set high
-        _delay (500);           //delay time for 0.5 seconds
+        _delay (500000);           //delay time for 0.5 seconds
         LATBbits.LATB6 = 0;    // pin 6 on Port B is set low
-        _delay (500);          // delay time for 0.5 seconds
+        _delay (500000);          // delay time for 0.5 seconds
     }
     return 0;
 }
